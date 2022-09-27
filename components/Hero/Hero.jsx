c21796951424c6498dd847bf5090356dc932e4ae
jupidev1586 <gianpaolobuono@gmail.com>
2022-09-27 17:18:18 +0200

    feat: slider hero css caption

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,15 +1,16 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'next/image';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 // import required modules
-import { EffectFade } from "swiper";
+import { EffectFade, Autoplay } from "swiper";
 import "swiper/css/effect-fade";
 // Import Swiper styles
 import 'swiper/css';
 
-import styles from '../../styles/Hero.module.scss';
+import styles from '../../styles/layout/Hero.module.scss';
 
 function Hero() {
   return (
@@ -21,27 +22,61 @@ function Hero() {
             <Swiper
               spaceBetween={0}
               effect={"fade"}
-              modules={[EffectFade]}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              modules={[EffectFade, Autoplay]}
             >
               <SwiperSlide>
-                <img src="https://picsum.photos/1920/600?1" alt="img" />
+                {/* <Image src="https://picsum.photos/1920/600?1" alt="img" layout="fill" width="100%" height="100%"/> */}
+                <img src="https://picsum.photos/1920/600?2" alt="img" />
                 <div className={`${styles.Caption}`}>
-                  <h3>
-
-                  </h3>
+                  <h2 className={`${'text-uppercase'}`}>
+                    Create <br />
+                    Your <br />
+                    J<span className={`${styles.azure}`}>o</span>u<span className={`${styles.orange}`}>r</span>n<span className={`${styles.green}`}>e</span>y
+                  </h2>
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <img src="https://picsum.photos/1920/600?2" alt="img" />
+                <div className={`${styles.Caption}`}>
+                  <h2 className={`${'text-uppercase'}`}>
+                    Create <br />
+                    Your <br />
+                    J<span className={`${styles.azure}`}>o</span>u<span className={`${styles.orange}`}>r</span>n<span className={`${styles.green}`}>e</span>y
+                  </h2>
+                </div>
               </SwiperSlide>
               <SwiperSlide>
                 <img src="https://picsum.photos/1920/600?3" alt="img" />
+                <div className={`${styles.Caption}`}>
+                  <h2 className={`${'text-uppercase'}`}>
+                    Create <br />
+                    Your <br />
+                    J<span className={`${styles.azure}`}>o</span>u<span className={`${styles.orange}`}>r</span>n<span className={`${styles.green}`}>e</span>y
+                  </h2>
+                </div>
               </SwiperSlide>
               <SwiperSlide>
                 <img src="https://picsum.photos/1920/600?4" alt="img" />
+                <div className={`${styles.Caption}`}>
+                  <h2 className={`${'text-uppercase'}`}>
+                    Create <br />
+                    Your <br />
+                    J<span className={`${styles.azure}`}>o</span>u<span className={`${styles.orange}`}>r</span>n<span className={`${styles.green}`}>e</span>y
+                  </h2>
+                </div>
               </SwiperSlide>
               <SwiperSlide>
-                <img src="https://picsum.photos/1920/600?5" alt="img" />
+                <img src="https://picsum.photos/1920/600?5" alt="img" /><div className={`${styles.Caption}`}>
+                  <h2 className={`${'text-uppercase'}`}>
+                    Create <br />
+                    Your <br />
+                    J<span className={`${styles.azure}`}>o</span>u<span className={`${styles.orange}`}>r</span>n<span className={`${styles.green}`}>e</span>y
+                  </h2>
+                </div>
               </SwiperSlide>
             </Swiper>
           </Col>
