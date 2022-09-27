98d0ebfec1ad97b732a75726826605d8e3fee7b1
Miyka96 <micaelaseminara@outlook.it>
2022-09-27 14:48:50 +0200

    fixed folder structure

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import styles from '../../styles/layout/Navbar.module.scss';
+
+const Navbar = () => {
+    return(
+        <nav className={styles.nav}>
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
