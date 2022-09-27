74f890f860385ea54aa10596a0f7837febe8c740
fefusphera <fefu.sphera@hotmail.com>
2022-09-27 15:42:12 +0200

    chore: added files

---
+++ components/ActivityCardList/ActivityCardList.jsx
@@ -0,0 +1,12 @@
+import ActivityCard from "../ActivityCard/";
+
+const ActivityCardList = () => {
+  return (
+    <div>
+      <h1>CardList</h1>
+      <ActivityCard />
+    </div>
+  );
+};
+
+export default ActivityCardList;
