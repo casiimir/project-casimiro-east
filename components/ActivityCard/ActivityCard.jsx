7b4e27c90739ae3a3185fb253419fff12d01ab7e
fefusphera <fefu.sphera@hotmail.com>
2022-09-28 10:42:24 +0200

    add ActivityCard

---
+++ components/ActivityCard/ActivityCard.jsx
@@ -0,0 +1,13 @@
+const ActivityCard = () => {
+  return (
+    <div className="activityCard">
+      <img
+        className="activityCardImg"
+        src="https://picsum.photos/200/200"
+        alt="img"
+      />
+    </div>
+  );
+};
+
+export default ActivityCard;
---
+++ components/ActivityCardList/index.jsx
@@ -0,0 +1,3 @@
+import ActivityCardList from "../ActivityCardList.jsx";
+
+export default ActivityCardList;
