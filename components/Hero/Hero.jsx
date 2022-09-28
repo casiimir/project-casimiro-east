c27d5a4a025094df648d6861bbc9d45772e563bf
jupidev1586 <gianpaolobuono@gmail.com>
2022-09-28 07:44:21 +0200

    feat: slider hero finished

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'next/image';
+// import Image from 'next/image';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 // import required modules
@@ -12,7 +12,64 @@ import 'swiper/css';
 
 import styles from '../../styles/layout/Hero.module.scss';
 
-function Hero() {
+
+
+
+
+const Hero = () => {
+
+  const photos = [
+    {
+      url: '/images/architecture-3121009_1920.jpg'
+    },
+    {
+      url: '/images/audience-1850119_1920.jpg'
+    },
+    {
+      url: '/images/aurora-1185464_1920.jpg'
+    },
+    {
+      url: '/images/beach-666122_1920.jpg'
+    },
+    {
+      url: '/images/beach-1852945_1920.jpg'
+    },
+    {
+      url: '/images/berchtesgaden-2928712_1920.jpg'
+    },
+    {
+      url: '/images/concert-6187717_1920.jpg'
+    },
+    {
+      url: '/images/desert-3217765_1920.jpg'
+    },
+    {
+      url: '/images/eiffel-tower-3349075_1920.jpg'
+    },
+    {
+      url: '/images/george-washington-bridge-2098351_1920.jpg'
+    }, 
+    {
+      url: '/images/hd-wallpaper-441853_1920.jpg'
+    },
+    {
+      url: '/images/maldives-1993704_1920.jpg'
+    }, 
+    {
+      url: '/images/mountain-2143877_1920.jpg'
+    },
+    {
+      url: '/images/palace-530055_1920.jpg'
+    },
+    {
+      url: '/images/portugal-3029665_1920.jpg'
+    },
+    {
+      url: '/images/rocks-1757593_1920.jpg'
+    }
+  ];
+
+
   return (
     
     <div className={`${styles.Hero}`}>
@@ -28,56 +85,31 @@ function Hero() {
               }}
               modules={[EffectFade, Autoplay]}
             >
-              <SwiperSlide>
-                {/* <Image src="https://picsum.photos/1920/600?1" alt="img" layout="fill" width="100%" height="100%"/> */}
-                <img src="https://picsum.photos/1920/600?2" alt="img" />
-                <div className={`${styles.Caption}`}>
-                  <h2 className={`${'text-uppercase'}`}>
-                    Create <br />
-                    Your <br />
-                    J<span className={`${styles.azure}`}>o</span>u<span className={`${styles.orange}`}>r</span>n<span className={`${styles.green}`}>e</span>y
-                  </h2>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="https://picsum.photos/1920/600?2" alt="img" />
-                <div className={`${styles.Caption}`}>
-                  <h2 className={`${'text-uppercase'}`}>
-                    Create <br />
-                    Your <br />
-                    J<span className={`${styles.azure}`}>o</span>u<span className={`${styles.orange}`}>r</span>n<span className={`${styles.green}`}>e</span>y
-                  </h2>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="https://picsum.photos/1920/600?3" alt="img" />
-                <div className={`${styles.Caption}`}>
-                  <h2 className={`${'text-uppercase'}`}>
-                    Create <br />
-                    Your <br />
-                    J<span className={`${styles.azure}`}>o</span>u<span className={`${styles.orange}`}>r</span>n<span className={`${styles.green}`}>e</span>y
-                  </h2>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="https://picsum.photos/1920/600?4" alt="img" />
-                <div className={`${styles.Caption}`}>
-                  <h2 className={`${'text-uppercase'}`}>
-                    Create <br />
-                    Your <br />
-                    J<span className={`${styles.azure}`}>o</span>u<span className={`${styles.orange}`}>r</span>n<span className={`${styles.green}`}>e</span>y
-                  </h2>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="https://picsum.photos/1920/600?5" alt="img" /><div className={`${styles.Caption}`}>
-                  <h2 className={`${'text-uppercase'}`}>
-                    Create <br />
-                    Your <br />
-                    J<span className={`${styles.azure}`}>o</span>u<span className={`${styles.orange}`}>r</span>n<span className={`${styles.green}`}>e</span>y
-                  </h2>
-                </div>
-              </SwiperSlide>
+
+            
+              {
+                photos.map( ( item, index, array ) => <SwiperSlide key={index}>
+                      
+                      <img src={item.url} alt="img" />
+
+                      <div className={`${styles.Caption}`}>
+
+                        <h2>
+                          Create <br />
+                          <small>Your</small> <br />
+                          J<span className={`${styles.azure}`}>o</span>u<span className={`${styles.orange}`}>r</span>n<span className={`${styles.green}`}>e</span>y
+                        </h2>
+
+                      </div>
+
+                    </SwiperSlide>
+                 
+                  
+                )
+              }
+              
+          
+
             </Swiper>
           </Col>
         </Row>
