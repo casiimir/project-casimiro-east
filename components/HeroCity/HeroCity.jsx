487ab980a2e0ff76f38a6b46dabe132a5262703d
jupidev1586 <gianpaolobuono@gmail.com>
2022-09-28 15:32:14 +0200

    feat hero-city added

--- components/HeroCity/HeroCity.jsx
+++ components/HeroCity/HeroCity.jsx
@@ -2,47 +2,27 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useState, useEffect } from "react";
-import Image from 'next/image';
 import axios from "axios";
 
 import styles from '../../styles/layout/HeroCity.module.scss';
 
 
+const HeroCity = ({ image, title, content }) => {
 
-
-
-const Hero = () => {
-
-  const [items, setItems] = useState([]);
-
-  const baseURL = "https://api.musement.com/api/v3/12";
   
-
-  useEffect( () => {
-    axios
-    .get(baseURL, {
-      params: {
-        
-      },
-    })
-    .then((res) => {
-      setItems(res.data[0].items);
-      console.table(res.data[0].items);
-    })
-    .catch((error) => {
-      console.log(error.response);
-    });
-  }, [])
-
   return (
     
     <div className={`${styles.HeroCity}`}>
       <Container fluid className={`${'g-0'}`}>
         <Row>
           <Col>
-          { 
-            items.map( item => <Image src={item.cover_image_url} alt="hero-city" layout='fill'/>)
-          }
+            <div className={`${styles.cont_image}`}>
+              <img src={image} alt="hero-city" />
+              <div className={`${styles.Caption}`}>
+                <h2>{title}</h2>
+                <p>{content}</p>
+              </div>
+            </div>
           </Col>
         </Row>
       </Container>
@@ -50,4 +30,4 @@ const Hero = () => {
   );
 }
 
-export default Hero;
+export default HeroCity;
