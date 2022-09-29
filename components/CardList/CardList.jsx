6ea66d87f5d14acc6bcb973fa5be4a06cf3efa3d
Cb040719 <claudiob0162@gmail.com>
2022-09-29 15:15:20 +0200

    cardlist minor fixes

--- components/CardList/CardList.jsx
+++ components/CardList/CardList.jsx
@@ -1,25 +1,22 @@
 import { useState, useEffect } from "react";
 import styles from "../../styles/layout/CardList.module.scss";
-// import { cityCardPlaceholder, experienceCardPlaceholder } from "../../utils/cardPlaceholder"
 import Card from "../Card/Card";
 import axios from "axios";
 
 const CardList = ({list}) => {
-  const citiesBaseURL = "https://api.musement.com/api/v3/cities";
-  const experiencesBaseURL = "https://api.musement.com/api/v3/activities";
-  const [experiences, setExperiences] = useState([]);
-
+  const baseURL = "https://api.musement.com/api/v3/activities";
+  const [displayList, setDisplayList] = useState([]);
 
   useEffect(() => {
     axios
-      .get(experiencesBaseURL, {
+      .get(baseURL, {
         params: {
           limit: 10,
           offset: 0,
         },
       })
       .then((res) => {
-        setExperiences(res.data.data);
+        setDisplayList(res.data.data);
       })
       .catch((error) => {
         console.log(error.response);
@@ -30,16 +27,16 @@ const CardList = ({list}) => {
   return (
     <div className={styles.CardList}>
       
-      {console.log(experiences)}
-      {experiences.map((experience) => {
+      {console.log(displayList)}
+      {displayList.map((item) => {
 
         if(list === "cities") {
 
-          return(<Card key={experience.city.id} name={experience.city.name} image={experience.city.cover_image_url}/>)
+          return(<Card key={item.city.id} name={item.city.name} image={item.city.cover_image_url}/>)
         }
         if(list === "experiences") {
           
-          return(<Card key={experience.uuid} name={experience.title} image={experience.cover_image_url}/>)
+          return(<Card key={item.city.id} name={item.title} image={item.cover_image_url}/>)
         }
       })}
     </div>
@@ -47,31 +44,3 @@ const CardList = ({list}) => {
 };
 
 export default CardList;
-
-
-// const CardList = ({list}) => {
-
-//   let displayList = [];
-//   let isVisible = "flex";
-
-//   if(list === "cities") {
-//       displayList.push(...cityCardPlaceholder);
-//   }
-//   if(list === "experiences") {
-//       displayList.push(...experienceCardPlaceholder);
-//       isVisible = "none";
-//   }
-
-//   return(
-//       <div className={styles.CardList}>
-
-//           {displayList.map((item) => <Card 
-//                                           key={item.id} 
-//                                           name={item.name} 
-//                                           image={item.img} 
-//                                           visibility={isVisible}
-//                                       />)}
-//           {console.log(displayList)}
-//       </div>
-//   )
-// }
