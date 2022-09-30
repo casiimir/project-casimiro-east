801630965cd6992638427ef4a4ec1afd9ab944a1
jupidev1586 <gianpaolobuono@gmail.com>
2022-09-30 11:09:48 +0200

    fix: css cards title

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,9 +1,9 @@
 import styles from "../../styles/layout/Card.module.scss";
 
-const Card = ({ name, image, visibility }) => {
+const Card = ({ name, image }) => {
   return (
     <div className={styles.Card} style={{backgroundImage:`url(${image})`}}>
-      <h2 style={{display:visibility}}>{name}</h2>
+      <h2>{name}</h2>
     </div>
   );
 };
