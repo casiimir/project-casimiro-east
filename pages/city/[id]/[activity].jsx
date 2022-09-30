4509e7938011269d22fd4ca20901c5452de0775c
Cb040719 <claudiob0162@gmail.com>
2022-09-30 13:19:46 +0200

    feat/activity page rendering

--- pages/city/[id]/[activity].jsx
+++ pages/city/[id]/[activity].jsx
@@ -1,18 +1,34 @@
 import { useRouter } from "next/router";
 import {getActivity} from '../../../api/api';
 import {useState, useEffect} from 'react';
+import styles from "../../../styles/layout/Activity.module.scss";
+import Navbar from "../../../components/Navbar";
+import Footer from "../../../components/Footer";
+
 const Activity = () => {
+
     const [activity, setActivity] = useState([]); 
     const router = useRouter();
     const activityId = router.query.activity;
+
     useEffect(() => {
         getActivity(activityId,setActivity);
     },[activityId])
+
     return(
-        <h1>
-            {activity?.title}
-        </h1>
+        <div className={styles.Activity}>
+            <Navbar/>
+            <h1>{activity?.title}</h1>
+            <img src={activity?.cover_image_url} alt="" />
+            <p>{activity?.description}</p>
+            <div className={styles.btnContainer}>
+                <h4>{activity?.retail_price?.formatted_iso_value}</h4>
+                <button>BOOK IT!</button>
+            </div>
+            <Footer/>
+        </div>
     )
 }
 
 export default Activity;
+
