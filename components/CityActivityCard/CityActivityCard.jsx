52c1aa61424c9e0dea1f4083a996050af9ef88b9
fefusphera <fefu.sphera@hotmail.com>
2022-09-30 18:40:24 +0200

    chore: add grid CSS

--- components/CityActivityCard/CityActivityCard.jsx
+++ components/CityActivityCard/CityActivityCard.jsx
@@ -1,23 +1,19 @@
 import styles from "../../styles/layout/CityActivityCard.module.scss";
 
-const CityActivityCard = () => {
+const CityActivityCard = ({ image, name, price, children }) => {
   return (
     <div className={styles.CityActivityCard}>
       <div className={styles.CityActivityCardDiv}>
-        <img
-          className={styles.CityActivityCardImg}
-          src="https://picsum.photos/200/300"
-          alt="image"
-        />
+        <img className={styles.CityActivityCardImg} src={image} alt={name} />
         <div className={styles.CityActivityCardText}>
-          <h1 className={styles.CityActivityCardTitle}>TITOLO</h1>
+          <h1 className={styles.CityActivityCardTitle}>{name}</h1>
           <div className={styles.CityActivityCardDescriptionDiv}>
-            <p className={styles.CityActivityCardDescription}>see more</p>
-            <p className={styles.CityActivityCardPrice}>$$$</p>
+            {/* <p className={styles.CityActivityCardDescription}>see more</p> */}
+            <p className={styles.CityActivityCardPrice}>{price}</p>
           </div>
         </div>
       </div>
-      {/* {children} */}
+      {children}
     </div>
   );
 };
