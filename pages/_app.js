df75ce3f7abf532b076d48fa1079b5cdc4cc0056
jupidev1586 <gianpaolobuono@gmail.com>
2022-10-06 12:33:58 +0200

    feat: cart new

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { CartContext, useCartState } from '../hooks/use-cart.js';
 import '../styles/globals.scss'
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const cart = useCartState();
+  return (
+    <CartContext.Provider value={cart}>
+      <Component {...pageProps} />
+    </CartContext.Provider>
+  );
 }
 
 export default MyApp
