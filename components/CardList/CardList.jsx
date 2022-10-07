4026d594608987ea33b8ac019bc1308ecc3a43dd
jupidev1586 <gianpaolobuono@gmail.com>
2022-10-07 12:01:28 +0200

    feat: css

--- components/CardList/CardList.jsx
+++ components/CardList/CardList.jsx
@@ -57,8 +57,8 @@ const CardList = ({ list, title }) => {
               modules={[ Navigation ]}
               className="swiperExp mb-5"
               breakpoints={{
-                0: {
-                  width: 480,
+                320: {
+                  width: 320,
                   slidesPerView: 1,
                 },
                 // when window width is >= 640px
